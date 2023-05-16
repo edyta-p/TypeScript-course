4ac9f84fa94b272e23132bcfb71224b3eed8a164
edyta pilorz <edytapilorz@gmail.com>
2023-05-16 13:18:12 +0200

    exercise with functions

---
+++ basics.js
@@ -0,0 +1,3 @@
+console.log("Hello!");
+console.log(Math.round(2.51654684548));
+var pi = 3.14159;
---
+++ basics.ts
@@ -0,0 +1,3 @@
+console.log("Hello!");
+console.log(Math.round(2.51654684548));
+let pi: number = 3.14159;
